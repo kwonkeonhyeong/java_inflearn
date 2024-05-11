e337eab7ed95af06e5b81649dd9fcb5b5474e23e
kwonkeonhyeong <rjsgud19566@gmail.com>
2024-05-11 15:57:49 +0900

    스프링 컨테이너와 스프링 빈

--- core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,15 +1,17 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    AppConfig appConfig = new AppConfig();
+
+    MemberService memberService = appConfig.memberService();
+    OrderService orderService = appConfig.orderService();
 
     @Test
     void createOrder() {
@@ -17,7 +19,7 @@ public class OrderServiceTest {
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId,"itemA",10000);
-        Assertions.assertThat(order.calculatePrice()).isEqualTo(9000);
+        Order order = orderService.createOrder(memberId,"itemA",20000);
+        Assertions.assertThat(order.calculatePrice()).isEqualTo(18000);
     }
 }
