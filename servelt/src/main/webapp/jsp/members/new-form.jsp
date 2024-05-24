29d1fadbea770e3ab5a3f4a376a8f50034e79d06
kwonkeonhyeong <rjsgud19566@gmail.com>
2024-05-24 23:18:01 +0900

    mvc1

---
+++ servelt/src/main/webapp/jsp/members/new-form.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: user
+  Date: 2024-05-23
+  Time: 오전 12:31
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<form action="/jsp/members/save.jsp" method="post">
+    username: <input type="text" name="username" />
+    age: <input type="text" name="age" />
+    <button type="submit">전송</button>
+</form>
+</body>
+</html>
---
+++ servelt/src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,43 @@
+package hello.servlet.domain.member;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+public class MemberRepositoryTest {
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Member member = new Member("hello", 20);
+        //when
+        Member saveMember = memberRepository.save(member);
+        //then
+        Member findMember = memberRepository.findById(saveMember.getId());
+        assertThat(findMember).isEqualTo(saveMember);
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Member member1 = new Member("뭉치", 5);
+        Member member2 = new Member("구름이", 2);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //when
+        List<Member> result = memberRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1,member2);
+    }
+}
